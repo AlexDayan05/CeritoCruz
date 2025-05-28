6f15925558319e748f5f86a5cb094e38d4aa4cde
AlexDayan05 <alexdayanrodriguezhernandez@gmail.com>
2025-05-28 12:50:14 -0400

    x

--- src/cero_cruz/Celda.java
+++ src/cero_cruz/Celda.java
@@ -1,5 +1,21 @@
 package cero_cruz;
 
 public class Celda {
+	public String texto;
+
+
+
+	public Celda(String texto) {
+		setTexto(texto);
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
 
 }
